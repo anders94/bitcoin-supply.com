b0a1fce527a26a0ec138ae434eb10b66c2f9708a
Anders Brownworth <anders@Anderss-MacBook-Pro.local>
2021-02-07 21:01:53 -0500

    add utils/update-running-totals.js

---
+++ utils/update-running-totals.js
@@ -0,0 +1,21 @@
+const db = require('../db');
+const config = require('../config');
+
+// recalculate current_total_supply starting from block 0
+
+const main = async () => {
+    await db.connect();
+    const latest_block = await db.getLatestBlock();
+    let previous_block = {current_total_supply: 0n};
+
+    for (let block_number = 0; block_number < latest_block; block_number++) {
+	const block = await db.getBlock(block_number);
+	block.current_total_supply = previous_block.current_total_supply + BigInt(block.new_supply);
+	await db.upsertBlock(block);
+	previous_block = block;
+    }
+    await db.end();
+
+}
+
+main();
