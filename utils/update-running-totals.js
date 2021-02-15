0e8947f76c244fff4d9ccfd6fe0e7cfa151b1ece
Anders Brownworth <anders-git@evantide.com>
2021-02-15 04:00:36 +0000

    update-running-totals.js: start from 0 or passed argument

--- utils/update-running-totals.js
+++ utils/update-running-totals.js
@@ -3,12 +3,12 @@ const config = require('../config');
 
 // recalculate current_total_supply starting from block 0
 
-const main = async () => {
+const main = async (start) => {
     await db.connect();
     const latest_block = await db.getLatestBlock();
     let previous_block = {current_total_supply: 0n};
 
-    for (let block_number = 0; block_number <= latest_block; block_number++) {
+    for (let block_number = start; block_number <= latest_block; block_number++) {
 	const block = await db.getBlock(block_number);
 	block.current_total_supply = previous_block.current_total_supply + BigInt(block.new_supply);
 	await db.upsertBlock(block);
@@ -18,4 +18,5 @@ const main = async () => {
 
 }
 
-main();
+const start = process.argv[2] ? Number(process.argv[2]) : 0;
+main(start);
