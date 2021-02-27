2da09efc0340f6f289094d19fc48434d335efd70
Anders Brownworth <anders-git@evantide.com>
2021-02-27 15:13:25 +0000

    redo backfill migrations

--- migrations/backfill/sqls/20210112204741-200k-down.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM inputs USING inputs i LEFT JOIN transactions t ON i.tx_hash = t.tx_hash WHERE t.block_number >= 100001 AND t.block_number <= 200000;
-DELETE FROM outputs USING outputs o LEFT JOIN transactions t ON o.tx_hash = t.tx_hash WHERE t.block_number >= 100001 AND t.block_number <= 200000;
-DELETE FROM transactions WHERE block_number >= 100001 AND block_number <= 200000;
-DELETE FROM blocks WHERE block_number >= 100001 AND block_number <= 200000;
---
+++ migrations/backfill/sqls/20210226040251-500k-down.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:12697f135f50bde22bd1b2b02da03fc92fb0a3f410a3bfc262905720980b771e
+size 36
