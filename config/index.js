9d7eed67569893661d179d1bad29b66ea97e24f9
Anders Brownworth <anders@Anderss-MacBook-Pro.local>
2021-03-31 22:40:59 -0400

    config/index.js: remove unused email settings

--- config/index.js
+++ config/index.js
@@ -8,15 +8,6 @@ module.exports = {
 	debug: false
     },
 
-    email: {
-        host: process.env.EMAILHOST || 'localhost',
-        port: process.env.EMAILPORT || 1025,
-        auth: {
-            user: process.env.EMAILUSER || 'project.1',
-            pass: process.env.EMAILPASSWORD || 'secret.1'
-        }
-    },
-
     paginationSize: 250,
 
     bitcoinRPC: {
